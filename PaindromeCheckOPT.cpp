8e4cebb279f8f43cb51f657fd1f7416aa50498af
Darren D'silva <crce.9367.aids@gmail.com>
2024-12-02 23:00:37 +0530

    Next

---
+++ PaindromeCheckOPT.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string result(string s)
+{
+    int left = 0;
+    int right = s.length()-1;
+    
+    string temp="";
+    string ans="";
+    
+    //Iterate the string and keep on adding to form a word
+    //If empty space is encountered then add the current word to the result
+    while (left <= right) {
+        char ch= s[left];
+        if (ch != ' ') {
+            temp += ch;
+        } else if (ch == ' ') {
+            if (ans!="") ans = temp + " " + ans;
+            else ans = temp;
+            temp = "";
+        }
+        left++;
+    }
+    
+    
+    if (temp!="") {
+        if (ans!="") ans = temp + " " + ans;
+        else ans = temp;
+    }
+    
+    return ans;    
+}
+int main()
+{
+    string st="everyday it do you if better gets It";
+    cout<<"Before : "<<endl;
+
+    cout<<st<<endl;
+
+    cout<<"After: "<<endl;
+
+    cout<<result(st);
+    return 0;
+}
